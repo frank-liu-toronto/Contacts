981e62575c5e41893a1fb7ba5360506a25c549bb
Frank <liu1000@gmail.com>
2023-02-20 18:20:22 -0500

    40. Implement SQLite Repository

---
+++ Plugins/Contacts.Plugins.DataStore.SQLLite/Constants.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Contacts.Plugins.DataStore.SQLLite
+{
+    public class Constants
+    {
+        public const string DatabaseFileName = "ContactsSQLite.db3";
+
+        public static string DatabasePath =>
+            Path.Combine(FileSystem.AppDataDirectory, DatabaseFileName);
+    }
+}
---
+++ Plugins/Contacts.Plugins.DataStore.SQLLite/ContactSQLiteRepository.cs
@@ -0,0 +1,68 @@
+﻿using Contacts.UseCases.PluginInterfaces;
+using SQLite;
+using Contact = Contacts.CoreBusiness.Contact;
+
+namespace Contacts.Plugins.DataStore.SQLLite
+{
+    // All the code in this file is included in all platforms.
+    public class ContactSQLiteRepository : IContactRepository
+    {
+        private SQLiteAsyncConnection database;
+
+        public ContactSQLiteRepository()
+        {
+            this.database = new SQLiteAsyncConnection(Constants.DatabasePath);            
+            this.database.CreateTableAsync<Contact>();
+        }
+
+        public async Task AddContactAsync(Contacts.CoreBusiness.Contact contact)
+        {
+            await this.database.InsertAsync(contact);
+        }
+
+        public async Task DeleteContactAsync(int contactId)
+        {
+            var contact = await GetContactByIdAsync(contactId);
+            if (contact != null && contact.ContactId == contactId)
+                await this.database.DeleteAsync(contact);
+        }
+
+        public async Task<Contacts.CoreBusiness.Contact> GetContactByIdAsync(int contactId)
+        {
+            return await this.database.Table<Contact>().Where(x => x.ContactId == contactId).FirstOrDefaultAsync();
+        }
+
+        public async Task<List<Contacts.CoreBusiness.Contact>> GetContactsAsync(string filterText)
+        {
+            if (string.IsNullOrWhiteSpace(filterText))
+            {
+                return await this.database.Table<Contact>().ToListAsync();
+            }
+
+            var contacts = await this.database.Table<Contact>().Where(x => !string.IsNullOrWhiteSpace(x.Name) && x.Name.StartsWith(filterText, StringComparison.OrdinalIgnoreCase))?.ToListAsync();
+
+            if (contacts == null || contacts.Count <= 0)
+                contacts = await this.database.Table<Contact>().Where(x => !string.IsNullOrWhiteSpace(x.Email) && x.Email.StartsWith(filterText, StringComparison.OrdinalIgnoreCase))?.ToListAsync();
+            else
+                return contacts;
+
+            if (contacts == null || contacts.Count <= 0)
+                contacts = await this.database.Table<Contact>().Where(x => !string.IsNullOrWhiteSpace(x.Phone) && x.Phone.StartsWith(filterText, StringComparison.OrdinalIgnoreCase))?.ToListAsync();
+            else
+                return contacts;
+
+            if (contacts == null || contacts.Count <= 0)
+                contacts = await this.database.Table<Contact>().Where(x => !string.IsNullOrWhiteSpace(x.Address) && x.Address.StartsWith(filterText, StringComparison.OrdinalIgnoreCase))?.ToListAsync();
+            else
+                return contacts;
+
+            return contacts;
+        }
+
+        public async Task UpdateContactAsync(int contactId, Contacts.CoreBusiness.Contact contact)
+        {
+            if (contactId == contact.ContactId)
+                await this.database.UpdateAsync(contact);
+        }
+    }
+}
