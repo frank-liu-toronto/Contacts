700baad10d1adf4328530a7e2e3668e51c9aa3a4
Frank <liu1000@gmail.com>
2023-03-04 20:49:31 -0500

    48. Create Contacts EndPoint

--- Contacts.WebApi/Program.cs
+++ Contacts.WebApi/Program.cs
@@ -1,4 +1,5 @@
 using Contacts.WebApi;
+using Contacts.WebApi.Models;
 using Microsoft.EntityFrameworkCore;
 
 var builder = WebApplication.CreateBuilder(args);
@@ -18,6 +19,12 @@ app.MapGet("/api/contacts", async (ApplicationDbContext db) =>
     var contacts = await db.Contacts.ToListAsync();
 
     return Results.Ok(contacts);
+});
+
+app.MapPost("/api/contacts", async (Contact contact, ApplicationDbContext db) =>
+{
+    db.Contacts.Add(contact);
+    await db.SaveChangesAsync();
 });
 
 app.Run();
